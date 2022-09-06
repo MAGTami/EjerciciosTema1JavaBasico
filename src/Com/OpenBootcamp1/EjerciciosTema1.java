9a450cc783cccf60380dec52bf2c8cef51e4ab4a
Crypto Guti <111974176+MAGTami@users.noreply.github.com>
2022-09-06 12:49:31 +0200

    Initial commit

---
+++ src/Com/OpenBootcamp1/EjerciciosTema1.java
@@ -0,0 +1,43 @@
+package Com.OpenBootcamp1;
+
+public class EjerciciosTema1 {
+
+    public static void main(String[] args) {
+        // 1. numericos
+
+        // 1.1 enteros
+        byte variable1 = 3;
+        System.out.println(variable1);
+        short variable2 = 13;
+        System.out.println(variable2);
+        int variable3 = 33;
+        System.out.println(variable3);
+        long variable4 = 300;
+        System.out.println(variable4);
+        long numeroTelefono = 333444555;
+        System.out.println(numeroTelefono);
+
+        // 1.2 decimales
+        float variable5 = 3.5f;
+        System.out.println(variable5);
+        double variable6 = 13.5d;
+        System.out.println(variable6);
+
+        // 2. booleano
+        boolean variable7 = false;
+        System.out.println(variable7);
+
+        boolean variable8 = true;
+        System.out.println(variable8);
+
+        // 3. texto
+        char variable9 = 'a';
+        System.out.println(variable9);
+
+        String variable10 = "Veni vidi vici";
+        System.out.println(variable10);
+
+
+    }
+
+}
